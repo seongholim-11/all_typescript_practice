684c1cf89200163b9bd0f5ffc7caf99064581e1f
seongho <damadu01@gmail.com>
2023-09-12 18:06:42 +0900

    타입도 변수에 담아쓰세요 type 키워드 써서 & readonly

--- ts/index.js
+++ ts/index.js
@@ -1,41 +1,10 @@
-/* let lastname: string = "lim";
-let age: number = 24;
-let hometown: string = "seoul";
+/* function hiname(n?:string):string{
+    if(typeof n === 'string'){
+        return `안녕하세요 ${n}`
+    }else{
+        return `이름이 없습니다.`
+    }
+}
 
-let favorite: {
-    title: string;
-    singer: string;
-} = {
-    title: "안녕",
-    singer: "폴킴",
-};
-
-let project: {
-    member: string[];
-    days: number;
-    started: boolean;
-} = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
-
-
- */
-/* // union type
-let 회원: number | string = 123;
-let 회원들: (number | string)[] = [123, "456"];
-let 오브젝트: { a: string | number } = { a: 123 };
-
-let 나이:unknown = 123; */
-let user = "kim";
-let age = undefined;
-let married = false;
-let 철수 = [user, age, married];
-let 학교 = {
-    score: [100, 97, 84],
-    teacher: "Phil",
-    friend: "John",
-};
-학교.score[4] = false;
-학교.friend = ["Lee", 학교.teacher];
+console.log(hiname('홍길동')); */
+const qwe = { name: "kim", phone: 123, email: "abc@naver.com", ch: true };
--- ts/index.ts
+++ ts/index.ts
@@ -1,48 +1,135 @@
-/* let lastname: string = "lim";
-let age: number = 24;
-let hometown: string = "seoul";
+/* function hiname(n?:string):string{
+    if(typeof n === 'string'){
+        return `안녕하세요 ${n}`
+    }else{
+        return `이름이 없습니다.`
+    }
+}
 
-let favorite: {
-    title: string;
-    singer: string;
-} = {
-    title: "안녕",
-    singer: "폴킴",
-};
+console.log(hiname('홍길동')); */
 
-let project: {
-    member: string[];
-    days: number;
-    started: boolean;
-} = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
+/* function count(n:string | number):number{
+    const a = String(n).length
+    return a
+}
 
-
+console.log("🚀 ~ file: index.ts:17 ~ count(a):", count(15616))
  */
 
-/* // union type
-let 회원: number | string = 123;
-let 회원들: (number | string)[] = [123, "456"];
-let 오브젝트: { a: string | number } = { a: 123 };
+/* function married(n: number, b: boolean, s: string): string {
+    let house: number;
+    let charming: number;
+    if (b) {
+        house = 500;
+    } else {
+        house = 0;
+    }
+    if (s === "상") {
+        charming = 100;
+    } else {
+        charming = 0;
+    }
 
-let 나이:unknown = 123; */
+    const total: number = n + house + charming;
 
-let user: string = "kim";
-let age: undefined | number = undefined;
-let married: boolean = false;
-let 철수: (string | undefined | number | boolean)[] = [user, age, married];
+    if (total >= 600) {
+        return "결혼가능";
+    } else {
+        return null;
+    }
+}
 
-let 학교: {
-    score: (number | boolean)[];
-    teacher: string;
-    friend: string | string[];
-} = {
-    score: [100, 97, 84],
-    teacher: "Phil",
-    friend: "John",
+console.log("🚀 ~ file: index.ts:43 ~ married(700, false, '중'):", married(100, false, '상')) */
+
+/* function isMarriagePossible(age: number, hasHouse: boolean, socialStatus: string): string | void {
+    const houseValue = hasHouse ? 500 : 0;
+    const charmingValue = socialStatus === "상" ? 100 : 0;
+
+    const totalValue = age + houseValue + charmingValue;
+
+    return totalValue >= 600 ? "결혼가능" : '';
+}
+
+console.log(isMarriagePossible(30, true, "하")); // 출력: null
+console.log(isMarriagePossible(25, true, "상")); // 출력: "결혼가능" */
+
+/* 
+function 내함수(x :number | string){
+    if(typeof x === 'string') return x + 1  //에러남 
+ }
+
+ console.log("🚀 ~ file: index.ts:62 ~  내함수('아'):",  내함수('아'))
+ */
+/* 
+function 변환(a: (number | string)[]): number[] {
+    let b:number[] = []
+    a.map((item, idx) => {
+        b[idx] = Number(item);
+    });
+    return b;
+}
+
+console.log("🚀 ~ file: index.ts:73 ~ 변환(['1', 2, '3']):", 변환(['1', 2, '3'])) */
+
+/* function 과목(a: { subject: string | string[] }) {
+    if (typeof a.subject === "string") {
+        const n = Object.keys(a.subject).length;
+        return a.subject
+    } else if (typeof a.subject === "object") {
+        const n = Object.keys(a.subject).length;
+        return a.subject[n-1]
+    }
+}
+*/
+
+/* type Person = {
+    name: string;
+    age: number;
 };
-학교.score[4] = false;
-학교.friend = ["Lee", 학교.teacher];
+
+let teacher: Person = { name: "john", age: 20 };
+
+type Girlfriend = {
+    readonly name: string;
+};
+
+const 여친: Girlfriend = {
+    name: "엠버",
+};
+
+여친.name = "우라"; */
+
+/* type Name = string;
+type Age = number;
+type Person = Name | Age;
+
+type PositionX = { x: number };
+type PositionY = { y: number };
+
+type NewType = PositionX & PositionY;
+
+let position: NewType = {
+    x: 10,
+    y: 20,
+};
+ */
+
+type PositionX = { x: number };
+type PositionY = { x: number };
+
+type NewType = PositionX & PositionY;
+
+type Abc = {
+    color?: string;
+    size: number;
+    readonly position: number[];
+};
+
+type Bcd = {
+    name: string;
+    phone: number;
+    email: string;
+    ch: boolean
+};
+
+const qwe: Bcd = { name: "kim", phone: 123, email: "abc@naver.com", ch:true };
